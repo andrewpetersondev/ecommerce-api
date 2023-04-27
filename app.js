ee49b79816a7fa810c8779ee0f763a0c49b55e33
Andrew Peterson <andrewpetersondev@gmail.com>
2023-04-26 19:41:12 -0500

    auth routes structure

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('E-Commerce API');
+require("dotenv").config()
+require("express-async-errors")
+
+const express = require("express")
+const app = express()
+
+// rest of the packages
+const morgan = require("morgan")
+
+const connectDB = require("./db/connect")
+
+// routers
+const authRouter = require("./routes/authRoutes")
+
+// middleware
+const NotFoundMiddleWare = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("e-commerce api")
+})
+
+app.use("/api/v1/auth", authRouter)
+
+app.use(NotFoundMiddleWare)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+const start = async () => {
+  try {
+    connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide name"],
+    maxlength: 50,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    maxlength: 50,
+    minlength: 3,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+})
+
+module.exports = mongoose.model("User", UserSchema)
