e1ac070b113b207d7e56a971c663c668742252f3
Andrew Peterson <andrewpetersondev@gmail.com>
2023-07-06 14:40:13 -0500

    up to video 295

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const { register, login, logout } = require("../controllers/authController")
 
+// the implied url is set to http://localhost:5000/api/v1/auth/
+
 router.post("/register", register)
 router.post("/login", login)
 router.get("/logout", logout)
