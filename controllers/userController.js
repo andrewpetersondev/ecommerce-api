a43cc555051b116f16c3d22da37a731b9f847726
Andrew Peterson <andrewpetersondev@gmail.com>
2023-07-07 15:17:05 -0500

    up to video 308

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,6 @@ const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
 const CustomError = require("../errors")
 const { createTokenUser, attachCookiesToResponse } = require("../utils/index")
-const { token } = require("morgan")
 
 const getAllUsers = async (req, res) => {
   console.log(req.user)
@@ -22,16 +21,36 @@ const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user })
 }
 
+// update user with findOneAndUpdate from video 306
+// const updateUser = async (req, res) => {
+//   const { email, name } = req.body
+//   if (!email || !name) {
+//     throw new CustomError.BadRequestError("please provide all values")
+//   }
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { email, name },
+//     { new: true, runValidators: true }
+//   )
+//   const tokenUser = createTokenUser(user)
+//   attachCookiesToResponse({ res, user: tokenUser })
+//   res.status(StatusCodes.OK).json({ user: tokenUser })
+// }
+
+// update user with user.save() from video 307
+// GOTCHA: this method changes the hashed password so users will have to login again
 const updateUser = async (req, res) => {
   const { email, name } = req.body
   if (!email || !name) {
     throw new CustomError.BadRequestError("please provide all values")
   }
-  const user = await User.findOneAndUpdate(
-    { _id: req.user.userId },
-    { email, name },
-    { new: true, runValidators: true }
-  )
+  const user = await User.findOne({ _id: req.user.userId })
+
+  user.email = email
+  user.name = name
+
+  await user.save()
+
   const tokenUser = createTokenUser(user)
   attachCookiesToResponse({ res, user: tokenUser })
   res.status(StatusCodes.OK).json({ user: tokenUser })
